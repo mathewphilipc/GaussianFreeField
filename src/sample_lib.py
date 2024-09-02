5c045e9b37b0868c019e4b4021dfeaf060cd776f
Mathew Calkins <mathewpcalkins@gmail.com>
2024-09-02 16:15:15 -0400

    partial clean up of ugly sampling code

--- src/sample_lib.py
+++ src/sample_lib.py
@@ -10,81 +10,88 @@ import random
 from scipy.sparse.linalg import eigs, eigsh
 from scipy.sparse import lil_matrix, dok_matrix, csc_matrix
 
-def linear_to_2D_coordinates(a, N):
+def linear_to_2D_coordinates(linear_coord, torus_len):
     """
     Projects a linear coordinate onto a 2D coordinate system on a 2-torus of length N.
 
     Parameters:
-    a (int): The linear coordinate.
-    N (int): The length of the 2-torus.
+    linear_coord (int): The linear coordinate.
+    torus_len (int): The length of the 2-torus.
 
     Returns:
     list: A list [x, y] representing the 2D coordinates, ranging from 0 to N-1.
     """
-    y = a % N
-    x = ((a - y) // N) % N
+    y = linear_coord % torus_len
+    x = ((linear_coord - y) // torus_len) % torus_len
     return [x, y]
 
-def linear_to_3D_coordinates(a, N):
+def linear_to_3D_coordinates(linear_coord, torus_len):
     """
     Projects a linear coordinate onto a 3D coordinate system on a 3-torus of length N.
 
     Parameters:
-    a (int): The linear coordinate.
-    N (int): The length of the 3-torus.
+    linear_coord (int): The linear coordinate.
+    torus_len (int): The length of the 3-torus.
 
     Returns:
     list: A list [x, y, z] representing the 3D coordinates, ranging from 0 to N-1.
     """
-    z = a % N
-    a = (a - z) // N
-    y = a % N
-    a = (a - y) // N
-    x = a % N
-    return [x,y,z]
+    remainder = linear_coord
+    z = remainder % torus_len
+    remainder = (remainder - z) // torus_len
+    y = remainder % torus_len
+    remainder = (remainder - y) // torus_len
+    x = remainder % torus_len
+    return [x, y, z]
 
 
-def torus_distance(arr1, arr2, N):
+def torus_distance(arr1, arr2, torus_len):
     """
     Computes the distance between two integer points on a mod-N torus.
 
     Parameters:
     arr1 (list): First list of coordinates.
     arr2 (list): Second list of coordinates.
-    N (int): The modulus representing the length of the torus.
+    torus_len (int): The length of the 3-torus.
 
     Returns:
     int: The torus distance between the two points.
     """
-    count = 0
-    min_len = min(len(arr1), len(arr2))
 
-    for i in range(min_len):
-        dist12 = (arr1[i] - arr2[i]) % N
-        dist21 = (arr2[i] - arr1[i]) % N
-        count += min(dist12, dist21)
+    if len(arr1) != len(arr2):
+        raise ValueError("Input arrays must have the same length.")
 
-    return count
+    distance = 0
+    for i in range(len(arr1)):
+        dist12 = (arr1[i] - arr2[i]) % torus_len
+        dist21 = (arr2[i] - arr1[i]) % torus_len
+        distance += min(dist12, dist21)
 
-def grid_distance(arr1, arr2, N):
+    return distance
+
+def grid_distance(arr1, arr2, grid_len):
     """
     Computes the grid distance between two integer points.
 
     Parameters:
     arr1 (list): First list of coordinates.
     arr2 (list): Second list of coordinates.
-    N (int): The length of the grid.
+    len (int): The length of the grid.
 
     Returns:
     int: The grid distance between the two points.
     """
-    count = 0
-    min_len = min(len(arr1), len(arr2))
 
-    for i in range(min_len):
-        count += abs(arr1[i] - arr2[i])
+    if len(arr1) != len(arr2):
+        raise ValueError("Input arrays must have the same length.")
 
-    return count
+    distance = 0
+    for i in range(len(arr1)):
+        dist12 = (arr1[i] - arr2[i])
+        dist21 = (arr2[i] - arr1[i])
+        distance += min(dist12, dist21)
+
+    return distance
 
 def are_neighbors_2D_torus(a, b, N):
     # Checks whether two numbers a b, when projected onto a 2D torus of length N, are neighbors.
@@ -105,36 +112,57 @@ def are_neighbors_2D_grid(a, b, N):
 def are_neighbors_3D_torus(a, b, N):
     # Checks whether two numbers a b, when projected onto a 2D torus of length N, are neighbors.
     # Torus coordinates are (x,y) each ranging from 0 to N - 1
+    """
+    Checks whether two linear coordinates correspond to neighboring 3-torus points.
+
+    Parameters:
+    a (int): First linear coordinate.
+    b (list): Second linear coordinate.
+    N (int): The length of the torus..
+
+    Returns:
+    bool: Whether the two points are torus neighbors.
+    """
     aArr = linear_to_3D_coordinates(a,N)
     bArr = linear_to_3D_coordinates(b,N)
     dist = torus_distance(aArr, bArr, N)
     return (dist == 1)
 
-def are_neighbors_3D_grid(a, b, N):
-    # Checks whether two numbers a b, when projected onto a 2D torus of length N, are neighbors.
-    # Torus coordinates are (x,y) each ranging from 0 to N - 1
-    aArr = linear_to_3D_coordinates(a,N)
-    bArr = linear_to_3D_coordinates(b,N)
-    dist = grid_distance(aArr, bArr, N)
+def are_neighbors_3D_grid(a, b, grid_len):
+    # Unused, for deletion
+    aCoords = linear_to_3D_coordinates(a, torus_len)
+    bCoords = linear_to_3D_coordinates(b, torus_len)
+    dist = grid_distance(aCoords, bCoords, torus_len)
     return (dist == 1)
 
-def all_3D_torus_neighbors(a, N):
-  # Given a vector index corresponding to a point in an NxNxN torus, returns the
-  # list of all neighbors' vector indices. We do this nicely, in O(1).
-  [x,y,z] = linear_to_3D_coordinates(a,N)
-  neighbors = []
-  # Increment up or down
-  for increment in {-1,1}:
-    for dim in {0,1,2}:
-      #print(f"Increment = {increment}")
-      #print(f"dim = {dim}")
-      new_coords = [x,y,z]
-      new_coords[dim] = ((new_coords[dim] + increment) % N)
-      #print("New coordinates:")
-      #print(new_coords)
-      new_neighbor = new_coords[2] + new_coords[1]*N + new_coords[0]*(N**2)
-      neighbors.append(new_neighbor)
-  return neighbors
+def all_3D_torus_neighbors(linear_coord, N):
+    """
+    Given a linear coordinate, finds neighboring linear coordinate of corresponding 3-torus point.
+
+    Parameters:
+    linear_coord (int): The linear coordinate.
+    torus_len (int): The length of the 3-torus.
+
+    Returns:
+    list: Linear coordinates of all neighbor points.
+    """
+
+    # Given a vector index corresponding to a point in an NxNxN torus, returns the
+    # list of all neighbors' vector indices. We do this nicely, in O(1).
+    [x,y,z] = linear_to_3D_coordinates(linear_coord,N)
+    neighbors = []
+    # Increment up or down
+    for increment in {-1,1}:
+        for dim in {0,1,2}:
+            #print(f"Increment = {increment}")
+            #print(f"dim = {dim}")
+            new_coords = [x,y,z]
+            new_coords[dim] = ((new_coords[dim] + increment) % N)
+            #print("New coordinates:")
+            #print(new_coords)
+            new_neighbor = new_coords[2] + new_coords[1]*N + new_coords[0]*(N**2)
+            neighbors.append(new_neighbor)
+    return neighbors
 
 # Given a vector index, convert it to equivalent N x N array index and count how
 # many boundary edges it has. That is, how many of the array index's neighbors
@@ -221,16 +249,23 @@ def sample_3D_torus(N, sample_mode):
         c = 0.0
         if (sample_mode == "linear"):
             c = random.uniform(0.0, 2.0)
+            print("We are doing linear sampling!")
         elif (sample_mode == "uniform"):
+            print("We are doing uniform sampling!")
             c = 1.0
+            print(f"Conductace = {c}")
         elif (sample_mode == "split"):
+            print("We are doing split sampling!")
             coin_flip = random.randint(0,1)
             if (coin_flip == 1):
+              print(f"Heads up value: {coin_flip}")
               c = 0.01
             else:
+              print(f"Heads down value: {coin_flip}")
               c = 1.99
         else:
             return
+        print(f"Conductance = {c}")
         H[u][u] = H[u][u] + c
         H[v][v] = H[v][v] + c
         H[u][v] = H[u][v] - c
@@ -279,4 +314,3 @@ def repeated_random_sample(N, sample_mode, output_dir, start_index, end_index):
       output_file = output_dir + '/microstate_' + str(i) + '.npy'
       print(f"Output file = {output_file}")
       np.save(output_file, microstate)
-      
