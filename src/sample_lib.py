7c48ec79e90e7fed9f223244c16e93ad3edb5a64
Mathew Calkins <mathewpcalkins@gmail.com>
2024-09-09 16:40:19 -0400

    remove unused 2D sampling logic

--- src/sample_lib.py
+++ src/sample_lib.py
@@ -10,21 +10,6 @@ import random
 from scipy.sparse.linalg import eigs, eigsh
 from scipy.sparse import lil_matrix, dok_matrix, csc_matrix
 
-def linear_to_2D_coordinates(linear_coord, torus_len):
-    """
-    Projects a linear coordinate onto a 2D coordinate system on a 2-torus of length N.
-
-    Parameters:
-    linear_coord (int): The linear coordinate.
-    torus_len (int): The length of the 2-torus.
-
-    Returns:
-    list: A list [x, y] representing the 2D coordinates, ranging from 0 to N-1.
-    """
-    y = linear_coord % torus_len
-    x = ((linear_coord - y) // torus_len) % torus_len
-    return [x, y]
-
 def linear_to_3D_coordinates(linear_coord, torus_len):
     """
     Projects a linear coordinate onto a 3D coordinate system on a 3-torus of length N.
@@ -93,22 +78,6 @@ def grid_distance(arr1, arr2, grid_len):
 
     return distance
 
-def are_neighbors_2D_torus(a, b, N):
-    # Checks whether two numbers a b, when projected onto a 2D torus of length N, are neighbors.
-    # Torus coordinates are (x,y) each ranging from 0 to N - 1
-    aArr = linear_to_2D_coordinates(a,N)
-    bArr = linear_to_2D_coordinates(b,N)
-    dist = torus_distance(aArr, bArr, N)
-    return (dist == 1)
-
-def are_neighbors_2D_grid(a, b, N):
-    # Checks whether two numbers a b, when projected onto a 2D torus of length N, are neighbors.
-    # Torus coordinates are (x,y) each ranging from 0 to N - 1
-    aArr = linear_to_2D_coordinates(a,N)
-    bArr = linear_to_2D_coordinates(b,N)
-    dist = grid_distance(aArr, bArr, N)
-    return (dist == 1)
-
 def are_neighbors_3D_torus(a, b, N):
     # Checks whether two numbers a b, when projected onto a 2D torus of length N, are neighbors.
     # Torus coordinates are (x,y) each ranging from 0 to N - 1
@@ -164,19 +133,8 @@ def all_3D_torus_neighbors(linear_coord, N):
             neighbors.append(new_neighbor)
     return neighbors
 
-# Given a vector index, convert it to equivalent N x N array index and count how
-# many boundary edges it has. That is, how many of the array index's neighbors
-# are off the array in Z^2?
-def count_boundary_edges_2D(N,k):
-  [x,y] = linear_to_2D_coordinates(k,N)
-  edgeCount = 0
-  if (x == 0 or x == N-1):
-    edgeCount = edgeCount + 1
-  if (y == 0 or y == N-1):
-    edgeCount = edgeCount + 1
-  return edgeCount
-
-# Same as above, but in 3D.
+# Given a vector index, convert it to equivalent NxNxN array index and count how
+# many boundary edges it has. 
 def count_boundary_edges_3D(N,k):
   [x,y,z] = linear_to_3D_coordinates(k,N)
   edgeCount = 0
@@ -187,55 +145,6 @@ def count_boundary_edges_3D(N,k):
   if (z == 0 or z == N-1):
     edgeCount = edgeCount + 1
   return edgeCount
-
-# Sample a GFF microstate on an NxN grid with Dirichlet boundary conditions.
-def sample_2D_grid(N):
-  start_time = time.perf_counter()
-  dim = 2
-  H = np.zeros([N**2,N**2])
-  for u in range(N**2):
-    for v in range(u+1,N**2):
-      u_coords = linear_to_2D_coordinates(u,N)
-      v_coords = linear_to_2D_coordinates(v,N)
-      if are_neighbors_2D_grid(u,v,N):
-        c = 1.0
-        H[u][u] = H[u][u] + c
-        H[v][v] = H[v][v] + c
-        H[u][v] = H[u][v] - c
-        H[v][u] = H[v][u] - c
-  construct_time = time.perf_counter()
-  # print("Construction time: {}".format(construct_time - start_time))
-
-  # Add interaction terms for edges connecting lattice poins to Dirichet-fixed
-  # boundary points
-  for k in range(N**2):
-    for count in range(count_boundary_edges_2D(N,k)):
-      c = 1.0
-      H[k][k] = H[k][k] + c
-  # These matrices diagonalize H, in the sense that D is diagonal, V is
-  # orthogonal, and H = R*D*R^T.
-  eigenvalues, eigenvectors = eigsh(H,N**2)
-  D = np.diag(eigenvalues).real
-  R = eigenvectors.real
-  diag_time = time.perf_counter()
-  # print("Diagonalization time: {}".format(diag_time - construct_time))
-
-  # Then each component (R^T*X)_i is an IID Gaussian with mu = 0 and
-  Xprime = np.zeros([N**2])
-  for v in range(N**2):
-    if abs(D[v][v] > 0.01):
-      mu = 0
-      sigma = np.sqrt(2*dim/D[v][v])
-      Xprime[v] = np.random.normal(mu,sigma)
-  X = R.dot(Xprime)
-  microstate = np.zeros([N,N])
-  for k in range(N**2):
-    [x,y] = linear_to_2D_coordinates(k,N)
-    microstate[x][y] = X[k]
-  sample_time = time.perf_counter()
-  # print("Sampling time: {}".format(sample_time - diag_time))
-
-  return microstate
 
 # Sample a zero-centered GFF microstate on an NxNxN torus.
 def sample_3D_torus(N, sample_mode):
